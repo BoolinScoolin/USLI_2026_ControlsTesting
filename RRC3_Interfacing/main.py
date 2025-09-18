6fa69f198b98b2a2e8511b5244d8579bb44b96a0
BoolinScoolin <colinriba@ufl.edu>
2025-09-18 19:54:53 -0400

    test

--- RRC3_Interfacing/main.py
+++ RRC3_Interfacing/main.py
@@ -1,4 +1,10 @@
 import serial
+
+import sys, signal
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
+
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 while True:
     line = ser.readline().decode('ascii', errors='ignore').strip()
