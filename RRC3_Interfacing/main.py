ef1f816a4c9f48cefc66185d1a1a52a9d37a5203
BoolinScoolin <colinriba@ufl.edu>
2025-09-18 19:58:17 -0400

    test

--- RRC3_Interfacing/main.py
+++ RRC3_Interfacing/main.py
@@ -3,10 +3,14 @@ import serial
 import sys, signal
 
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-
+print("Check 1")
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+print("Check 2")
+
 while True:
     line = ser.readline().decode('ascii', errors='ignore').strip()
+    print("Check 3")
     if line:
         print(line)
+        print("Check 4")
