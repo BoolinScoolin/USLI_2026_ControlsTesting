06568bca9f720f8e3ebb1f05cd75a5c45e19a21a
BoolinScoolin <colinriba@ufl.edu>
2025-09-18 02:50:09 -0400

    test

--- interface_testing.py
+++ interface_testing.py
@@ -1,15 +1,21 @@
-import time
-import board, busio
-import adafruit_lsm6ds.lsm6ds3trc as LSM6DS
+import socket  # used for live streaming through TCP
+import time  # used for delays
+import board, busio  # circuitpython
+import adafruit_lsm6ds.lsm6ds3trc as LSM6DS  # adafruit
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = LSM6DS.LSM6DS3TRC(i2c)
 
+HOST = "192.168.1.1"   # your laptop's IP on the Pi–laptop Ethernet link
+PORT = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
 try:
     while True:
-        # Gyro values in radians/sec
         gx, gy, gz = sensor.gyro
-        print(f"Gyro: x={gx:.3f}, y={gy:.3f}, z={gz:.3f}")
-        #time.sleep(0.2)  # adjust speed (0.2s = 5 Hz)
+        line = f"{gx:.6f},{gy:.6f},{gz:.6f}\n"
+        sock.sendall(line.encode("utf-8"))
 except KeyboardInterrupt:
-    print("\nStopped by user.")
+    sock.close()
