98cf58f19fba5d119db0103fa25ec0dd606d1164
BoolinScoolin <colinriba@ufl.edu>
2025-09-19 19:46:58 -0400

    test

--- BMP390_Interfacing/main.py
+++ BMP390_Interfacing/main.py
@@ -1,13 +1,15 @@
 import time
 import board
 import adafruit_bmp3xx
+
 i2c = board.I2C()
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
 
-while True:
+# while True:
 
-    print("Pressure: {:6.1f}".format(bmp.pressure))
-    print("Temperature: {:5.2f}".format(bmp.temperature))
+print("Pressure: {:6.1f}".format(bmp.pressure))
+print("Temperature: {:5.2f}".format(bmp.temperature))
 
-    time.sleep(0.2)
+#    time.sleep(0.2)
+
