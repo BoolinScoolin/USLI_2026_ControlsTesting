7d98f58d180fd106001493d2730c3fa150ee4ad9
BoolinScoolin <colinriba@ufl.edu>
2025-09-19 19:43:04 -0400

    test

--- BMP390_Interfacing/main.py
+++ BMP390_Interfacing/main.py
@@ -4,5 +4,14 @@ import adafruit_bmp3xx
 i2c = board.I2C()
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
-print("Pressure: {:6.1f}".format(bmp.pressure))
-print("Temperature: {:5.2f}".format(bmp.temperature))
+
+try:
+    while True:
+
+        print("Pressure: {:6.1f}".format(bmp.pressure))
+        print("Temperature: {:5.2f}".format(bmp.temperature))
+
+        time.sleep(0.2)
+
+except KeyboardInterrupt:
+    sock.close()
