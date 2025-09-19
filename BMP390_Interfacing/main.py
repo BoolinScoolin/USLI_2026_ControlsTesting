2f2113720fdeb018b335dbb76a7bba822f3c7786
BoolinScoolin <colinriba@ufl.edu>
2025-09-19 19:59:56 -0400

    test

--- BMP390_Interfacing/main.py
+++ BMP390_Interfacing/main.py
@@ -1,9 +1,17 @@
 import time
+print('1')
+
 import board
+print('2')
+
 import adafruit_bmp3xx
+print('3')
 
 i2c = board.I2C()
+print('4')
+
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+print('5')
 
 
 # while True:
